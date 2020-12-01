1bacc4bc5dc8b72a558b6b1cf9f87821779b2265
Vehicle Researcher <>
2020-12-02 00:17:59 +0900

    openpilot v0.8.0

--- selfdrive/controls/lib/events.py
+++ selfdrive/controls/lib/events.py
@@ -209,11 +209,21 @@ def wrong_car_mode_alert(CP: car.CarParams, sm: messaging.SubMaster, metric: boo
 
 def standstill_alert(CP, sm, metric):
   elapsed_time = sm['pathPlan'].standstillElapsedTime
-  return Alert(
-    "잠시 멈춤 (경과시간: %d초)" % elapsed_time,
-    "",
-    AlertStatus.normal, AlertSize.small,
-    Priority.LOW, VisualAlert.none, AudibleAlert.none, .0, .1, .1, alert_rate=0.5)
+  elapsed_time_min = elapsed_time // 60
+  elapsed_time_sec = elapsed_time - (elapsed_time_min * 60)
+
+  if elapsed_time_min == 0:
+    return Alert(
+      "잠시 멈춤 (경과시간: %02d초)" % (elapsed_time_sec),
+      "",
+      AlertStatus.normal, AlertSize.small,
+      Priority.LOW, VisualAlert.none, AudibleAlert.none, .0, .1, .1, alert_rate=0.5)
+  else:
+    return Alert(
+      "잠시 멈춤 (경과시간: %d분 %02d초)" % (elapsed_time_min, elapsed_time_sec),
+      "",
+      AlertStatus.normal, AlertSize.small,
+      Priority.LOW, VisualAlert.none, AudibleAlert.none, .0, .1, .1, alert_rate=0.5)
 
 EVENTS: Dict[int, Dict[str, Union[Alert, Callable[[Any, messaging.SubMaster, bool], Alert]]]] = {
   # ********** events with no alerts **********
