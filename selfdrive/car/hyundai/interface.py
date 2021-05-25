d8f97f25b64689c6bb8afd59b55dfe6f05f3d8a2
Vehicle Researcher <>
2021-05-25 09:39:50 +0900

    openpilot v0.8.4

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -220,7 +220,7 @@ class CarInterface(CarInterfaceBase):
     ret.brakeMaxV = [0.7, 3.0]   # max brake allowed
 
     ret.longitudinalTuning.kpBP = [0., 4., 9., 17., 23., 31.]
-    ret.longitudinalTuning.kpV = [1.6, 1.1, 1.0, 0.6, 0.4, 0.3]
+    ret.longitudinalTuning.kpV = [1.6, 1.1, 1.0, 0.55, 0.35, 0.3]
     ret.longitudinalTuning.kiBP = [0., 4., 9., 17., 23., 31.]
     ret.longitudinalTuning.kiV = [0.21, 0.15, 0.13, 0.08, 0.07, 0.06]
 
--- selfdrive/car/hyundai/spdctrlLong.py
+++ selfdrive/car/hyundai/spdctrlLong.py
@@ -113,9 +113,9 @@ class SpdctrlLong(SpdController):
             elif CS.clu_Vanz > 40 and lead_objspd < -2 and int(CS.clu_Vanz) <= int(CS.VSetDis) and int(CS.clu_Vanz) >= dRel*2.2 and 1 < dRel < 149: # 유지거리 범위 외 감속 조건 앞차 감속중 현재속도/2 아래로 거리 좁혀졌을 때 상대속도에 따라 점진적 감소
                 self.seq_step_debug = "SS>VS,v>40,-1"
                 lead_wait_cmd, lead_set_speed = self.get_tm_speed( CS, max(20, 70+(lead_objspd*2)), -1)
-            elif 60 > CS.clu_Vanz > 30 and lead_objspd < -2 and int(CS.clu_Vanz) <= int(CS.VSetDis) and int(CS.clu_Vanz) >= dRel*0.85 and 1 < dRel < 149:
+            elif 60 > CS.clu_Vanz > 30 and lead_objspd < -3 and int(CS.clu_Vanz) <= int(CS.VSetDis) and int(CS.clu_Vanz) >= dRel*0.85 and 1 < dRel < 149:
                 self.seq_step_debug = "SS>VS,60>v>30,-1"
-                lead_wait_cmd, lead_set_speed = self.get_tm_speed( CS, max(20, 200-(abs(lead_objspd**3))), -1)
+                lead_wait_cmd, lead_set_speed = self.get_tm_speed( CS, max(20, 250-(abs(lead_objspd**3))), -1)
             elif 7 < int(CS.clu_Vanz) < 30 and lead_objspd < 0 and CS.VSetDis > 30:
                 self.seq_step_debug = "SS>VS,30이하"
                 lead_wait_cmd, lead_set_speed = self.get_tm_speed( CS, 20, -5)
