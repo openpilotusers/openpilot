b7863d9a76d747f0069a545434f967eadb6de052
openpilotusers <>
2021-06-22 08:24:22 +0900

    openpilot v0.8.6

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -228,9 +228,9 @@ class CarInterface(CarInterfaceBase):
     ret.longitudinalTuning.deadzoneBP = [0., 9., 17.]
     ret.longitudinalTuning.deadzoneV = [0., 0.1, 0.05]
     ret.longitudinalTuning.kdBP = [0., 4., 9., 17., 23., 31.]
-    ret.longitudinalTuning.kdV = [0.8, 0.8, 0.8, 0.8, 0.45, 0.35]
+    ret.longitudinalTuning.kdV = [0.8, 0.7, 0.75, 0.75, 0.45, 0.35]
     ret.longitudinalTuning.kfBP = [0., 4., 9., 17., 23., 31.]
-    ret.longitudinalTuning.kfV = [1., 1.1, 1.1, 1., 0.9, 0.7]
+    ret.longitudinalTuning.kfV = [1., 1., 1., 1., 0.9, 0.7]
 
     ret.enableCamera = True
     ret.enableBsm = 0x58b in fingerprint[0]
